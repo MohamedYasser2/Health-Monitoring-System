fc3b2f054ecf14694c8d5d184c6b29fe21bdb973
mark74384 <58529681+mark74384@users.noreply.github.com>
2022-03-15 21:42:17 +0200

    client UDP

--- Client/src/com/company/Main.java
+++ Client/src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+    }
+    public static void send_packet(String s) throws IOException {
+        int server_port = 3500;
+        DatagramSocket datagramSocket = new DatagramSocket();
+        InetAddress server_address = InetAddress.getLocalHost();
+        DatagramPacket datagramPacket = new DatagramPacket(s.getBytes(StandardCharsets.UTF_8) , s.length() , server_address , server_port );
+        datagramSocket.send(datagramPacket);
+        datagramSocket.close();
     }
 }
