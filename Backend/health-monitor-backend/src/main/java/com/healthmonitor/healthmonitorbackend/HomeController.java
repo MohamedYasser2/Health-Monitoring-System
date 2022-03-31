f29e908eef43c48ac3b050039ee2e69bdc3f97b4
MohamedYasser2 <mohamed.y.ghanem@gmail.com>
2022-03-31 23:22:48 +0200

    backend

--- Backend/health-monitor-backend/src/main/java/com/healthmonitor/healthmonitorbackend/HomeController.java
+++ Backend/health-monitor-backend/src/main/java/com/healthmonitor/healthmonitorbackend/HomeController.java
@@ -1,18 +1,20 @@
 package com.healthmonitor.healthmonitorbackend;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping("/api")
 @CrossOrigin
 public class HomeController {
-    @GetMapping("/get/statistics/")
+    @GetMapping("/getstatistics")
     public ArrayList<String> getStatistics(@RequestParam String startDate, @RequestParam String endDate) {
         System.out.println("I am hereeeeeee");
-        return new ArrayList<>();
+        ArrayList<String> test=new ArrayList<>();
+        test.add(startDate);
+        test.add(endDate);
+        return test;
     }
 }
+//
