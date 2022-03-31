f36d25939c307624a375f4e9950a240bbbebe94f
MohamedYasser2 <mohamed.y.ghanem@gmail.com>
2022-03-31 23:19:10 +0200

    frontend

--- Frontend/health-monitor/src/App.vue
+++ Frontend/health-monitor/src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+    Big Data
+    </v-app-bar>
+    <v-main>
+      <v-btn rounded @click="GetStatistics()">Get statistics </v-btn>
+      <div v-for="stat in statistics" :key="stat" >
+        <h3> {{stat}} </h3>
+      </div>
+      <div>
+        <v-row>
+        <v-date-picker v-model="from"></v-date-picker>
+        <v-date-picker v-model="to"></v-date-picker>
+        </v-row>
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+      
+  },
+
+  data() {
+    return {
+      from : "",
+      to : "",
+      statistics : [],
+    };
+  },
+  methods:{
+    GetStatistics(){
+      axios.get('http://localhost:8080/api/getstatistics',{
+        params : {
+          startDate : this.from,
+          endDate : this.to
+        }
+      })
+        .then((response) => {
+            console.log(response.data)
+            this.statistics = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+};
+</script>
